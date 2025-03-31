6f32044253a8aa98bbaad96dbd2eb84108ee9f5d
Swapnil Ingle <ingleswapnil2004@gmail.com>
2025-03-31 22:02:30 +0530

    Add initial CSS styles and Vercel configuration
    - Created a new CSS file for styling the trading view dashboard with a dark theme.
    - Added a Vercel configuration file to set up deployment with Python and static file routing.

--- trading_view_dashboard/trading_view_dashboard/settings.py
+++ trading_view_dashboard/trading_view_dashboard/settings.py
@@ -138,8 +138,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Ensure STATICFILES_DIRS includes the static directory
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
