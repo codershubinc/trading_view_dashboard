3c7291f868e3061296e67d4b1e9ab03b514dda37
Swapnil Ingle <ingleswapnil2004@gmail.com>
2025-03-31 21:53:53 +0530

    Ensure STATICFILES_DIRS includes the static directory for proper static file handling

--- trading_view_dashboard/trading_view_dashboard/settings.py
+++ trading_view_dashboard/trading_view_dashboard/settings.py
@@ -140,7 +140,10 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# Ensure STATICFILES_DIRS includes the static directory
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
