e99f77246be1f8183c8d2240f1fe0a3d2da30cdc
Swapnil Ingle <ingleswapnil2004@gmail.com>
2025-03-31 22:18:37 +0530

    Refactor STATIC_URL to a single path and set STATIC_ROOT for static file management

--- trading_view_dashboard/trading_view_dashboard/settings.py
+++ trading_view_dashboard/trading_view_dashboard/settings.py
@@ -138,11 +138,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = [
-    "/static/",
-    "/staticfiles/",
-    'https://trade.codershubinc.tech/static/',
-]
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Ensure STATICFILES_DIRS includes the static directory
 STATICFILES_DIRS = [
