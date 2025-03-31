6fd53254e11df7ef9ad74a4e858040b36e12bef8
Swapnil Ingle <ingleswapnil2004@gmail.com>
2025-03-31 20:25:23 +0530

    Update static files settings: change STATIC_ROOT path to 'staticfiles' for consistency

--- trading_view_dashboard/trading_view_dashboard/settings.py
+++ trading_view_dashboard/trading_view_dashboard/settings.py
@@ -32,9 +32,11 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
 ]
 
-# Static files settings for production
+# Static files settings
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Ensure DEBUG is False for production
 DEBUG = True
