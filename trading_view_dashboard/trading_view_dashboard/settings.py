f50d4613b912009338755e9d83bbcc15483f70f6
Swapnil Ingle <ingleswapnil2004@gmail.com>
2025-03-31 20:29:41 +0530

    Refactor static files settings: comment out static files configuration for clarity

--- trading_view_dashboard/trading_view_dashboard/settings.py
+++ trading_view_dashboard/trading_view_dashboard/settings.py
@@ -32,10 +32,10 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
 ]
 
-# Static files settings
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# # Static files settings
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Ensure DEBUG is False for production
