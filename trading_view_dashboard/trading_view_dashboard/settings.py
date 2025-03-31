05e21df71ea64fb7e5c0d563dbf591f8e4da4605
Swapnil Ingle <ingleswapnil2004@gmail.com>
2025-03-31 22:16:52 +0530

    Update STATIC_URL to support multiple paths and adjust STATICFILES_DIRS for improved static file handling

--- trading_view_dashboard/trading_view_dashboard/settings.py
+++ trading_view_dashboard/trading_view_dashboard/settings.py
@@ -138,13 +138,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = [
+    "/static/",
+    "/staticfiles/",
+    'https://trade.codershubinc.tech/static/',
+]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Ensure STATICFILES_DIRS includes the static directory
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
     BASE_DIR / 'static',
-    '/var/www/static/',
+    '/var/www/.codershubinc.tech/static/',
     '/home/user/static/',
 ]
 
