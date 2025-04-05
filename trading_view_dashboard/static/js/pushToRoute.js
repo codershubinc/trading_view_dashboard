d68f6246857dc9b98e1855d0b057cbe2edc69d9a
Swapnil Ingle <ingleswapnil2004@gmail.com>
2025-04-06 03:22:29 +0530

    Update pushToRoute.js to include graph type in URL and format plot.html to display currency for 52 Week High/Low

--- trading_view_dashboard/static/js/pushToRoute.js
+++ trading_view_dashboard/static/js/pushToRoute.js
@@ -1,5 +1,5 @@
 const handleSubmit = (event) => {
     event.preventDefault();
     const symbol = document.getElementById('symbol').value
-    return window.location.href = `/plot/${symbol}`
+    return window.location.href = `/plot/${symbol}/?graph=high`
 }
